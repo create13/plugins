aa1130bfae67aec4f285dde3240da53509515afd
李小乐er <1974931363@qq.com>
2018-01-17 17:51:18 +0800

    webpack打包项目

---
+++ loginedWebpack/js/index.js
@@ -0,0 +1,6 @@
+require("../css/mobase750.css");
+require("../css/nf_login.css");
+window.$ = require("jquery");
+require("./dpr.js");
+require("./logined.js");
+require("../page/logined.html");
---
+++ loginedWebpack/webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+	entry:'./js/index.js',
+	output:{
+		path:__dirname+'/dist',
+		filename:'bundle.js'
+	},
+	module:{
+		loaders:[
+			{
+				test:/\.css$/,
+				loader:'style-loader!css-loader'
+
+			},
+			{
+				test: /\.(jpg|png|gif|svg|jpeg)$/,
+				loader: 'url-loader?limit=8192&name=images/[hash:8].[name].[ext]',
+				options:{
+					publicPath:'./images'
+				}
+
+					
+				
+			},
+			{
+				/*test: /\.(jpg|png|gif|svg)$/,*/
+				test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+				loader: 'file-loader?limit=8192&name=images/[hash:8].[name].[ext]',
+				options:{
+					publicPath:'./images'
+				}
+				
+			},
+			{
+        		test: /\.html$/,
+        		loader: 'file-loader?name=[path][hash:8][name].[ext]!extract-loader!html-loader'
+        		/*loader: "html-loader"*/
+      		}
+		]
+	}
+}
